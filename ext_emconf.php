28751560c84457d0136985136a361844766e027d
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-27 16:50:00 +0000

    Release dhdbmm 1.0.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dhdbmm/trunk@32661 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "dhdbmm".
 #
-# Auto generated 30-01-2010 23:14
+# Auto generated 27-04-2010 18:49
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'oliverklee.de',
-	'version' => '0.0.1',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'ameos_formidable' => '2.0.0-',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"621a";s:20:"class.ux_tx_dhdb.php";s:4:"643c";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"2893";s:29:"tests/ux_tx_dhdb_testcase.php";s:4:"f01c";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"2418";s:20:"class.ux_tx_dhdb.php";s:4:"643c";s:16:"ext_autoload.php";s:4:"d6fc";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"2893";s:14:"doc/manual.sxw";s:4:"c2ef";}',
 	'suggests' => array(
 	),
 );
